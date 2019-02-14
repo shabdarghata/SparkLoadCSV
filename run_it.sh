ee5852f64add3bc4851148fc72b23ba061d232cd
shabdarghata <shabdar.ghata@gmail.com>
2019-02-14 01:37:50 +0000

    Changes in POM.XML

---
+++ run_it.sh
@@ -0,0 +1,1 @@
+/projects/spark-2.2.1-bin-hadoop2.7/bin/spark-submit --class info.hadooptutorials.LoadCSV --master local[2] /projects/SparkLoadCSV/target/SparkLoadCSV-1.0.jar
---
+++ src/main/java/info/hadooptutorials/LoadCSV.java
@@ -0,0 +1,57 @@
+package info.hadooptutorials;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
+
+public class LoadCSV {
+  public static void main(String[] args) throws Exception {
+    SparkSession spark = SparkSession
+      .builder()
+      .master("local[*]")
+      .appName("LoadCSV")
+      .getOrCreate();
+
+    StructType schema = new StructType()
+.add("policyID", "string")
+.add("statecode", "string")
+.add("county", "string")
+.add("eq_site_limit", "long")
+.add("hu_site_limit", "long")
+.add("fl_site_limit", "long")
+.add("fr_site_limit", "long")
+.add("tiv_2011", "long")
+.add("tiv_2012", "long")
+.add("eq_site_deductible", "long")
+.add("hu_site_deductible", "long")
+.add("fl_site_deductible", "long")
+.add("fr_site_deductible", "long")
+.add("point_latitude", "string")
+.add("point_longitude", "string")
+.add("line", "string")
+.add("construction", "string")
+.add("point_granularity", "long");
+
+    Dataset<Row> df = spark.read()
+    .option("mode", "DROPMALFORMED")
+    .schema(schema)
+    //.csv("/projects/SparkLoadCSV/FL_insurance_sample.csv");    
+    .csv("hdfs:///localhost/SparkLoadCSV/FL_insurance_sample.csv");    
+
+    df.createOrReplaceTempView("insurance");
+
+    Dataset<Row> sqlResult = spark.sql(
+        "SELECT statecode, county, SUM(hu_site_limit) as TotalSiteLimit, COUNT(policyID) as NoOfPolicies" 
+            + " FROM insurance GROUP BY statecode, county order by NoOfPolicies DESC");
+    
+    sqlResult.show(1000,false); //for testing
+
+sqlResult = spark.sql(
+        "SELECT count(*)" 
+            + " FROM insurance");
+    
+    sqlResult.show(1000,false); //for testing
+
+
+    }
+}
